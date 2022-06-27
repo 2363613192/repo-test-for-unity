4ebb1dca5e4738c41542efd88a263ec1fddde446
guil21 <2363613192@qq.com>
2022-06-27 19:35:48 +0800

    debug

--- Assets/Scenes/test.cs
+++ Assets/Scenes/test.cs
@@ -6,6 +6,7 @@ public class test : MonoBehaviour
 {
     void Start()
     {
+        // debug
         Debug.Log("initial test by guil");
         Debug.Log("git test by guil");
         //Debug.Log("branch test by guil");

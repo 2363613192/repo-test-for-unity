faf14e9344998f5b44422cae4d6d3b24b6227e24
guil21 <2363613192@qq.com>
2022-06-27 19:10:33 +0800

    修改演示

--- Assets/Scenes/test.cs
+++ Assets/Scenes/test.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class test : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("initial test by guil");
-        Debug.Log("git test by guil");
-        Debug.Log("branch test by guil");
+        //Debug.Log("initial test by guil");
+        //Debug.Log("git test by guil");
+        //Debug.Log("branch test by guil");
     }
 
-    // Update is called once per frame
     void Update()
     {
         
